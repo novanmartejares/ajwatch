577931593d05d4364513a8909bed21625aa6dc80
novanmartejares <novantejares@gmail.com>
2024-04-06 23:05:12 +0800

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "K-Next",
+  name: "AJ-Watch",
   description:
     "Experience the enchanting world of Korean drama like never before with our cutting-edge app designed for true K-Drama aficionados. Dive into a captivating universe of compelling storylines, heart-fluttering romances, and breathtaking cinematography. Our app is your passport to an extensive library of the latest and classic Korean drama series, all curated to cater to your unique taste. Whether you're a seasoned K-Drama enthusiast or just beginning your journey into this mesmerizing genre, our app promises an unparalleled streaming experience. Immerse yourself in the rich storytelling, vibrant characters, and cultural allure of Korean dramas, all conveniently accessible at your fingertips. Elevate your entertainment experience – download our app today and embark on an unforgettable K-Drama adventure!",
   url: "https://kd.rohi.dev",
   links: {
-    twitter: "https://twitter.com/gneiru",
-    github: "https://github.com/gneiru",
+    twitter: "https://twitter.com/novanmar",
+    github: "https://github.com/novanmartejares",
   },
   mainNav: [
     {
@@ -17,6 +17,14 @@ export const siteConfig: SiteConfig = {
     {
       title: "popular",
       href: "/popular",
+    },
+    {
+      title: "Anime",
+      href: "https://papawatch.site",
+    },
+    {
+      title: "Movies",
+      href: "https://papavanz.com",
     },
   ],
 };
